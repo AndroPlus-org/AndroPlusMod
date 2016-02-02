ca17ab2cb47c6cf666dfad56f4980f818d419931
AndroPlus-org <hosigumayuugi@gmail.com>
2016-02-02 23:02:37 +0900

    Added screenshot delay remover
    big thanks to MohammadAG and bin456789 !

--- app/src/main/java/org/androplus/wedymod/NotificationiconPatcher.java
+++ app/src/main/java/org/androplus/wedymod/NotificationiconPatcher.java
@@ -5,6 +5,8 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 import android.content.res.XModuleResources;
 import android.content.res.XResources;
 import android.os.Build;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.text.TextUtils;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -21,6 +23,7 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 	private static XSharedPreferences preference = null;
 	private static String modulePath = null;
 	private static final String TEXTVIEW = "android.widget.TextView";
+	private static final long DELAY = 0L;
 
 	public NotificationiconPatcher() {
 	}
@@ -196,6 +199,7 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 		{
 			case "android":
 				hookPowerManager(lpparam);
+				hookScreenshotDelay(lpparam);
 				break;
 			case "com.sonymobile.prototypeprotection":
 				hookPrototypeProtection(lpparam);
@@ -302,5 +306,70 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 			}
 		}
 		}
+	
+	public void hookScreenshotDelay(LoadPackageParam lpparam)
+		{
+		boolean isDelayRM = preference.getBoolean("key_remove_screenshot_delay", false);
+		if(isDelayRM){
+			try
+			{
+			 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+		            XposedHelpers.findAndHookMethod("com.android.server.policy.PhoneWindowManager",
+		                    lpparam.classLoader, "getScreenshotChordLongPressDelay", XC_MethodReplacement.returnConstant(DELAY));
+
+		        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+		            XposedHelpers.findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManagerr",
+		                    lpparam.classLoader, "getScreenshotChordLongPressDelay", XC_MethodReplacement.returnConstant(DELAY));
+
+		        else {
+		            final boolean isLowerThanJB = Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN;
+		            XposedHelpers.findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManager",
+		                    lpparam.classLoader, "interceptScreenshotChord", new XC_MethodReplacement() {
+		                        @Override
+		                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+
+		                            //only JB+ has this field, so assume it's true under JB
+		                            boolean mScreenshotChordEnabled = isLowerThanJB || XposedHelpers.getBooleanField(param.thisObject, "mScreenshotChordEnabled");
+
+		                            boolean mVolumeDownKeyTriggered = XposedHelpers.getBooleanField(param.thisObject,
+		                                    "mVolumeDownKeyTriggered");
+		                            boolean mPowerKeyTriggered = XposedHelpers.getBooleanField(param.thisObject,
+		                                    "mPowerKeyTriggered");
+		                            boolean mVolumeUpKeyTriggered = XposedHelpers.getBooleanField(param.thisObject,
+		                                    "mVolumeUpKeyTriggered");
+
+		                            if (mScreenshotChordEnabled
+		                                    && mVolumeDownKeyTriggered && mPowerKeyTriggered && !mVolumeUpKeyTriggered) {
+		                                long mVolumeDownKeyTime = XposedHelpers.getLongField(param.thisObject,
+		                                        "mVolumeDownKeyTime");
+		                                long mPowerKeyTime = XposedHelpers.getLongField(param.thisObject,
+		                                        "mPowerKeyTime");
+		                                long SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS = XposedHelpers.getLongField(param.thisObject,
+		                                        "SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS");
+
+		                                final long now = SystemClock.uptimeMillis();
+		                                if (now <= mVolumeDownKeyTime + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS
+		                                        && now <= mPowerKeyTime + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS) {
+		                                    XposedHelpers.setBooleanField(param.thisObject, "mVolumeDownKeyConsumedByScreenshotChord", true);
+		                                    XposedHelpers.callMethod(param.thisObject, "cancelPendingPowerKeyAction");
+
+		                                    Handler mHandler = (Handler) XposedHelpers.getObjectField(param.thisObject, "mHandler");
+		                                    Runnable mScreenshotChordLongPress = (Runnable) XposedHelpers.getObjectField(param.thisObject,
+		                                            "mScreenshotChordLongPress");
+		                                    mHandler.postDelayed(mScreenshotChordLongPress, DELAY);
+		                                }
+		                            }
+		                            return null;
+		                        }
+		                    });
+		        }
+		        }
+			catch(Throwable t)
+			{
+				XposedBridge.log(t);
+			}
+		}
+		}
+	
 	
 }
